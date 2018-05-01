ce413826aebebfd11e28b37febea0628b50c0d26
Erin Wild <eewild@gmail.com>
2018-05-01 18:49:43 -0400

    Use pytest instead

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -8,27 +8,12 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    word_value = 0
-    for character in word.upper():
-        if character in LETTER_SCORES:
-            word_value += LETTER_SCORES[character]
-    return word_value
+    return sum(LETTER_SCORES.get(character, 0) for character in word.upper())
 
-def max_word_value(word_list=None):
+def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    if word_list is None:
-        word_list = load_words()
-
-    word_with_max_value = ''
-    max_value = 0
-    for word in word_list:
-        word_value = calc_word_value(word)
-        if word_value > max_value:
-            word_with_max_value = word
-            max_value = word_value
-
-    return word_with_max_value
+    return max(words, key=calc_word_value)
 
 if __name__ == '__main__':
-    pass # run unittests to validate
+    pass # run unit tests to validate
